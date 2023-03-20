987f05464cd07ef7d88f20198157cfa05ed78e65
residentsIeeper <senia27sh2@gmail.com>
2023-03-20 10:16:06 +0200

    init

---
+++ src/test/java/arsen/musicarchive/MusicArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package arsen.musicarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
