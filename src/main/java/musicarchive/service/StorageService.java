21af678842b2c7e12c777f978b1725b7d432de7b
residentsIeeper <senia27sh2@gmail.com>
2023-03-22 10:59:29 +0200

    Create environment variables for credentials

--- src/main/java/musicarchive/service/StorageService.java
+++ src/main/java/musicarchive/service/StorageService.java
@@ -8,6 +8,7 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,17 +17,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.lang.System.getenv;
-
 @Slf4j
 @Service
 public class StorageService {
 
     private final AmazonS3 bucket;
+    Dotenv dotenv = Dotenv.configure().load();
 
     public StorageService() {
+
         AWSCredentialsProvider credentials = new AWSStaticCredentialsProvider(
-                new BasicAWSCredentials(getenv("ACCESS_KEY"), getenv("SECRET_KEY"))
+                new BasicAWSCredentials(dotenv.get("ACCESS_KEY"), dotenv.get("SECRET_KEY"))
         );
 
         bucket = AmazonS3ClientBuilder
@@ -34,7 +35,7 @@ public class StorageService {
                 .withCredentials(credentials)
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(
-                                getenv("S3_URL"), getenv("REGION")
+                                dotenv.get("S3_URL"), dotenv.get("REGION")
                         )
                 )
                 .build();
@@ -42,7 +43,7 @@ public class StorageService {
 
     public List<String> getSongFileNames() {
 
-        ListObjectsV2Result result = bucket.listObjectsV2(getenv("BUCKET_NAME"));
+        ListObjectsV2Result result = bucket.listObjectsV2(dotenv.get("BUCKET_NAME"));
         List<S3ObjectSummary> objects = result.getObjectSummaries();
 
         return objects.stream()
@@ -54,7 +55,7 @@ public class StorageService {
             ObjectMetadata objectMetadata = new ObjectMetadata();
             objectMetadata.setContentType(file.getContentType());
             bucket.putObject(new PutObjectRequest(
-                    getenv("BUCKET_NAME"),
+                    dotenv.get("BUCKET_NAME"),
                     file.getOriginalFilename(),
                     file.getInputStream(),
                     objectMetadata)
